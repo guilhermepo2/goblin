948e11661877dadd84100bc73ca3f05eb407a744
Guilherme Oliveira <guilherme.061@gmail.com>
2023-02-27 13:27:15 -0800

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#define GUEEPO2D_MAIN
+#include <gueepo2d.h>
+
+class Sample : public gueepo::Application {
+public:
+	Sample() : Application("sample ", 640, 360) {}
+	~Sample() {}
+};
+
+gueepo::Application* gueepo::CreateApplication() {
+	return new Sample();
+}
