67d9cf11675f064dcb010ad475b8f8608012a60b
Guilherme Oliveira <guilherme.061@gmail.com>
2023-02-28 20:41:18 -0800

    adding lua
    maybe I should fork the lua repo or do my own, idk

---
+++ src/assets/test.lua
@@ -0,0 +1,3 @@
+function TestFunction()
+    _Log("Hello From Lua!")
+end
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,15 @@
 #include <gueepo2d.h>
 #include "goblin.h"
 
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+};
+
 static gbln::ResourceManager g_ResourceManager;
 static gbln::GameWorld g_GameWorld;
+static lua_State* g_luaState;
 
 class GoblinApplication : public gueepo::Application {
 public:
@@ -19,8 +26,29 @@ private:
     gueepo::OrtographicCamera* m_camera;
 };
 
+int lua_Log(lua_State* L) {
+    const char* message = lua_tostring(L, -1);
+    LOG_INFO("[lua] {0}", message);
+    return 0;
+}
+
 void GoblinApplication::Application_OnInitialize() {
     m_camera = new gueepo::OrtographicCamera(640, 360);
+    g_luaState = luaL_newstate();
+    luaL_openlibs(g_luaState);
+    lua_register(g_luaState, "_Log", lua_Log);
+
+    int r = luaL_dofile(g_luaState, "./assets/test.lua");
+    if(r != LUA_OK) {
+        std::string errormsg = lua_tostring(g_luaState, -1);
+        LOG_ERROR("lua error: {0}", errormsg);
+    } else {
+        lua_getglobal(g_luaState, "TestFunction");
+        if(lua_isfunction(g_luaState, -1)) {
+            lua_pcall(g_luaState, 0, 0, 0);
+        }
+    }
+
     g_ResourceManager.LoadResource("./assets/resources.json");
 
     gbln::Entity* goblinKing = g_GameWorld.AddEntity("Goblin King");
@@ -45,7 +73,6 @@ void GoblinApplication::Application_OnUpdate(float DeltaTime) {
 }
 
 void GoblinApplication::Application_OnRender() {
-
     gueepo::Renderer::BeginFrame(*m_camera);
     gueepo::Renderer::Clear(0.5f, 0.1f, 0.1f, 1.0f);
     g_GameWorld.Render();
